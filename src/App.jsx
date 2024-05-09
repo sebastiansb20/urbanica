b036a371087119c1c17a61b4153e69e9c5b92bd0
Sebastian <sebastiansanchezbcl@gmail.com>
2024-05-09 16:39:32 -0300

    inicioInfoDisciplinas

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
-
+import TextoProyectos from './components/vpTexto/TextoProyectos';
+import PilaresInicio from './components/pilaresInicio/PilaresInicio';
 function App() {
   
 
@@ -10,8 +11,9 @@ function App() {
       <>
         <Navbar />
         <Hero />
-      </>
-
+        <TextoProyectos />
+        <PilaresInicio />
+    </>
   )
 }
 
