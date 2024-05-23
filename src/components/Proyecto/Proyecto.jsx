5ce4e8e977fc5efd3f1b034110097e5a99ee6544
Sebastian <sebastiansanchezbcl@gmail.com>
2024-05-23 18:52:56 -0300

    proyectos

--- src/components/Proyecto/Proyecto.jsx
+++ src/components/Proyecto/Proyecto.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Grid, Box } from '@material-ui/core';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import './Proyecto.css'
 
-const Proyecto = ({nombreProyecto, detalleProyecto, imgProyecto}) =>{
+const Proyecto = ({nombreProyecto, detalleProyecto, imgProyecto, pos}) =>{
     return(
-    <div className="dvSeccionProyecto">
-
+        pos === 1 ? (
+            <div className="dvSeccionProyecto">
+                
                 <div>
                     <Grid container>
 
                         <Grid item xs={12} sm={4} md={5}>
                             <Box>
                                 <div className='dvTextoProyecto'>
-                                    <h4>{nombreProyecto} </h4>
-                                    <h2>{detalleProyecto} </h2>
-                                    <Button id="btnverproyecto" variant="contained">VER PROYECTO</Button>
+                                    <h2>{nombreProyecto} </h2>
+                                    <h4>{detalleProyecto} </h4>
+                                    <button id="btnverproyecto" >VER PROYECTO</button>
                                 </div>
                             </Box>
                         </Grid>
@@ -30,8 +31,37 @@ const Proyecto = ({nombreProyecto, detalleProyecto, imgProyecto}) =>{
 
                     </Grid>
                 </div>
-
             </div>
+            ) : (
+                <div className="dvSeccionProyecto">
+                    <div>
+                        <Grid container>
+
+                            <Grid item xs={12} sm={8} md={7}>
+                                <Box>
+                                    <div className="dvImagenProyecto">
+                                        <img src={imgProyecto} alt='LunasDelParqueUrbanicaUruguay' id="imgLunas" />
+                                    </div>
+                                </Box>
+                            </Grid>
+                            
+                            <Grid item xs={12} sm={4} md={5}>
+                                <Box>
+                                    <div className='dvTextoProyecto'>
+                                        <h4>{nombreProyecto} </h4>
+                                        <h2>{detalleProyecto} </h2>
+                                        <button id="btnverproyecto">VER PROYECTO</button>
+                                    </div>
+                                </Box>
+                            </Grid>
+
+                            
+
+                        </Grid>
+                    </div>
+                </div>
+            )
+            
     )
 }
 
--- src/pages/Proyectos/Proyectos.jsx
+++ src/pages/Proyectos/Proyectos.jsx
@@ -3,7 +3,11 @@ import Navbar from '../../components/navbar/Navbar';
 import './Proyectos.css'
 import Proyecto from '../../components/Proyecto/Proyecto';
 import imgLunas from '../../assets/images/proyectos/LunasDelParqueUrbanicaUruguay.jpg'
-
+import imgAraza from '../../assets/images/proyectos/ArazaUrbanicaUruguay.png'
+import imgJacaranda from '../../assets/images/proyectos/ViaJacarandaUruguay.png'
+import imgVilladelSol from '../../assets/images/proyectos/CroquisVilladelSolUruguay.jpg'
+import imgMuseo from '../../assets/images/proyectos/MuseoSkyspaceUrbanicaUruguay.jpg'
+import imgHotel from '../../assets/images/proyectos/HotelSkyspaceUrbanicaUruguay.jpeg';
 const Proyectos = () =>{
     
     return(
@@ -21,8 +25,22 @@ const Proyectos = () =>{
                 <h2>ARQUITECTURA</h2>
             </div>
 
-            <Proyecto nombreProyecto='"Lunas del Parque"' detalleProyecto='EDIFICIO RESIDENCIAL' imgProyecto={imgLunas} />
-            <Proyecto nombreProyecto='"Arazá"' detalleProyecto='EDIFICIO RESIDENCIAL' imgProyecto={imgLunas}/>
+            <Proyecto nombreProyecto='"Lunas del Parque"' detalleProyecto='EDIFICIO RESIDENCIAL' imgProyecto={imgLunas} pos={1} />
+            <Proyecto nombreProyecto='"Arazá"' detalleProyecto='EDIFICIO RESIDENCIAL' imgProyecto={imgAraza} pos={0}/>
+
+            <div className="dvheadingPr">
+                <h2>URBANISMO</h2>
+            </div>
+
+            <Proyecto nombreProyecto='"Vía Jacarandá"' detalleProyecto='BARRIO PRIVADO' imgProyecto={imgJacaranda} pos={1}/>
+            <Proyecto nombreProyecto='"Villa del Sol"' detalleProyecto='URBANIZACIÓN' imgProyecto={imgVilladelSol} pos={0}/>
+
+            <div className="dvheadingPr">
+                <h2>PAISAJISMO</h2>
+            </div>
+
+            <Proyecto nombreProyecto='"SKYSPACE TA KHUT BY JAMES TURRELL"' detalleProyecto='MUSEO' imgProyecto={imgMuseo} pos={1}/>
+            <Proyecto nombreProyecto='"The Grand Hotel"' detalleProyecto='HOTEL' imgProyecto={imgHotel} pos={0}/>
         </>
     );
 
