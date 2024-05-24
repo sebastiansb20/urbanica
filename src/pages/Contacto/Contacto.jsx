c6ca6645742d5c6fc9c578b335e7c84280fa74aa
Sebastian <sebastiansanchez.b20@gmail.com>
2024-05-24 01:44:02 -0300

    formularioContacto

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -1,13 +1,103 @@
-import React from 'react';
+import { React, useState} from 'react';
 import NavBar from '../../components/navbar/Navbar'
+import { FormControl } from '@mui/base/FormControl';
+import { Input, inputClasses } from '@mui/base/Input';
+import './Contacto.css'
+import Button from '@mui/material/Button';
+import {Box, Grid} from '@material-ui/core'
+
+
 
 const Contacto = () =>{
+
+    
+const [texto, setTexto] = useState('');
+
+const handleClick = ()=>{
+    if(texto.trim() === ''){
+        alert('El email está vacío');
+    }
+}
+
+const handleChange = (event) =>{
+    setTexto(event.target.value);
+}
+
+
     return(
         <>
         <NavBar />
 
+        <div className='dvSeccionContacto'>
+            <h1>CONTACTO</h1>
+        </div>
         
-        
+        <div className="row input-container">
+			<div className="col-xs-12">
+				<div className="styled-input wide">
+					<input type="text" required />
+					<label>Nombre</label> 
+				</div>
+			</div>
+			<div className="col-md-6 col-sm-12">
+				<div className="styled-input">
+					<input type="text" required onChange={handleChange} />
+					<label>Email</label> 
+				</div>
+			</div>
+			<div className="col-md-6 col-sm-12">
+				<div className="styled-input" style={{float:'right'}}>
+					<input type="text" required />
+					<label>Teléfono</label> 
+				</div>
+			</div>
+			<div className="col-xs-12">
+				<div className="styled-input wide">
+					<textarea required></textarea>
+					<label>Mensaje</label>
+				</div>
+			</div>
+			<div className="col-xs-12">
+                <Button id='btnEnviar' variant="contained" onClick={handleClick}>ENVIAR</Button>
+			</div>
+	    </div>
+        <div className='dvPrincipalesContactos'>
+        <Grid container className='gridContainerContactos'>
+            <Grid item xs={2} >
+
+                <Box className={'boxContacto'}>
+                    <h3>ARQUITECTURA</h3>
+                    
+                </Box>
+
+            </Grid>
+            <Grid item xs={2} >
+
+                <Box className={'boxContacto'}>
+                    <h3>URBANISMO</h3>
+                   
+                </Box>
+                
+            </Grid>
+            <Grid item xs={2} >
+
+                <Box className={'boxContacto'}>
+                    <h3>URBANISMO</h3>
+                   
+                </Box>
+                
+            </Grid>
+            <Grid item xs={2} >
+
+                <Box className={'boxContacto'}>
+                    <h3>PAISAJISMO</h3>
+                    
+                </Box>
+
+            </Grid>
+        </Grid>       
+
+        </div>
         </>
     )
 }
