42eb9d3b1500029de966791235b9cb73e127d63e
Sebastian <sebastiansanchez.b20@gmail.com>
2024-05-27 18:19:32 -0300

    inicioLunasdelParque

---
+++ src/pages/ViaJacaranda/ViaJacaranda.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import NavBar from '../../components/navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+// import heroJacaranda from '../../assets/images/viajacaranda/'
+
+
+const ViaJacaranda = () =>{
+
+    return(
+        <>
+        {/* <NavBar />
+        <div className='dvPageJacaranda'>
+
+            <h1>LUNAS DEL PARQUE</h1>
+
+            <img src={heroJacaranda} alt='ViaJacarandaUrbanicaUruguay'/>
+
+            <Grid container>
+
+                <Grid item xs={12} sm={4} md={5}>
+                    <Box>
+                        <img src={jacarandaImagenDos} alt='ViaJacarandaUrbanicaUruguay'/>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={4} md={5}>
+                    <Box>
+                        <p>Se trata de un proyecto diferente dentro de la trama urbana, con vistas al Parque Centenario, <br/>la tipología recrea el concepto de casa en altura.</p>
+                    </Box>
+                </Grid>
+
+            </Grid>
+
+
+        </div>
+
+        <Footer /> */}
+        </>
+    )
+}
+
+export default ViaJacaranda;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,8 @@ import Inicio from '../pages/Inicio/Inicio'
 import Proyectos from '../pages/Proyectos/Proyectos'
 import Equipo from '../pages/Equipo/Equipo'
 import Contacto from '../pages/Contacto/Contacto'
+import ViaJacaranda from '../pages/ViaJacaranda/ViaJacaranda'
+import LunasdelParque from '../pages/LunasdelParque/LunasdelParque'
 
 function RoutesS() {
     return (
@@ -14,6 +16,8 @@ function RoutesS() {
           <Route exact path="/proyectos" element={<Proyectos />} />
           <Route exact path="/equipo" element={<Equipo />} />
           <Route exact path="/contacto" element={<Contacto />} />
+          <Route exact path="/via-jacaranda" element={<ViaJacaranda />} />
+          <Route exact path="/lunasdelparque" element={<LunasdelParque />} />
         </Routes>
       </Router>
     );
