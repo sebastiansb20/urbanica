1f55b016c10214035a571b1bec605ba4f4dc70a6
Sebastian <sebastiansanchez.b20@gmail.com>
2024-06-01 01:19:03 -0300

    ubicAraza

--- src/pages/Araza/Araza.jsx
+++ src/pages/Araza/Araza.jsx
@@ -5,6 +5,7 @@ import logoAraza from '../../assets/images/araza/logoAraza.png'
 import arazaFrente from '../../assets/images/araza/arazaFrenteHero.png'
 import arazaFruto from '../../assets/images/araza/arazaFruto.png'
 import ubicacionAraza from '../../assets/images/araza/arazaUbicacion.png'
+import ubicacion from '../../assets/images/araza/ubicacion.png'
 
 const Araza = () =>{
 
@@ -38,41 +39,50 @@ const Araza = () =>{
         <>
         <Navbar />
         <div className="container">
+
             <div className="dvLogoAraza">
               <img id='imgLogoAraza' src={logoAraza} style={{margin:'0 auto'}}alt='ArazaUrbanicaUruguay' />  
               <h1 style={{display:'none'}}>Araza</h1>
             </div>
-            
-            <div className="dvSeccionHeroAraza">
-                    <img id='imgFrenteAraza'src={arazaFrente} alt='ArazaUrbanicaUruguay' />
-            </div>
-            
+
             <div className="dvImagenTexto">
                 <h2>UNA NUEVA FORMA DE VIVIR EN LA CIUDAD</h2>
             </div>
 
+            <div className="dvSeccionHeroAraza">
+                <img id='imgFrenteAraza'src={arazaFrente} alt='ArazaUrbanicaUruguay' />
+            </div>
+
             <div className="dvSeccionTextoDerecha">
-                <h2 className='headingVegetal hVDerecha'>ARQUITECTURA + PAISAJE: EL VEGETAL <br/><span className='spanParteF'>COMO PARTE FUNDAMENTAL </span><br/>DEL DISEÑO ARQUITECTÓNICO</h2>
+                <h2 className='hdSeccionTexto hdTextoDerecha'>ARQUITECTURA + PAISAJE: EL VEGETAL<span className='spanParteF'>COMO PARTE FUNDAMENTAL </span>DEL DISEÑO ARQUITECTÓNICO</h2>
                 <p>Un edificio que se convierte en un espacio vivo, con una cuidadosa selección del vegetal que procura recuperar biodiversidad y generar identidad local.
                     Un pequeño aporte a la ecología urbana que generará impactos positivos en la ciudad y a quienes lo habiten.</p>
             </div>
             <div className="dvSeccionTextoIzquierda">
-                <h2 className='headingVegetal hVIzquierda'>ARAZÁ ES UN ESPACIO DONDE LA ARQUITECTURA Y LA NATURALEZA CONVIVEN</h2>
+                <h2 className='hdSeccionTexto hdTextoIzquierda'>ARAZÁ ES UN ESPACIO DONDE LA ARQUITECTURA Y LA NATURALEZA CONVIVEN</h2>
                 <p>En la ciudad la presencia del vegetal se ve reducida a pocos espacios: plazas, canteros centrales de avenidas, 
                     algunos jardines particulares, perdiendo cada vez más el contacto con los ritmos naturales y los beneficios que aporta la naturaleza.</p>
             </div>
-            
+        
             <div className="dvSeccionFruto">
-                <img src={arazaFruto} alt='ArazaUrbanicaUruguay' />
+                <img id='imgArazaFruto' src={arazaFruto} alt='ArazaUrbanicaUruguay' />
                 <h2>Arazá incorpora la vegetación al diseño arquitectónico como un componente más del mismo.</h2>
             </div>
+            <div class="h-divider">
+                        <div class="shadow"></div>
+                    </div>
             <div className="dvSeccionUbicacion">
-                <h2>San José de Mayo, Uruguay</h2>
+
+                <div className="dvHeadingUbicacion">
+                    <img id='imgUbicacion' src={ubicacion} alt='ArazaUrbanicaUruguay' />
+                    <h2>San José de Mayo, Uruguay</h2>
+                </div>
+                
                 <p>Se encuentra ubicado estratégicamente en el corazón de la ciudad, sobre calle Vidal, en un entorno tranquilo y familiar.</p>
-                <p>Lejos del ruido pero con rápido acceso a todos los servicios que ofrece el centro. A una cuadra de la renovada Avda. Luis Alberto de Herrera, que se transformó en el principal eje comercial de la ciudad.
-                    A pasos de todos los servicios públicos, peatonales, colegios y de las 4 principales plazas de San José de Mayo.
+                <p id='pubD'>Lejos del ruido pero con rápido acceso a todos los servicios que ofrece el centro. A una cuadra de la renovada Avda. Luis Alberto de Herrera, que se transformó en el principal eje comercial de la ciudad.
+                    <br /><span>A pasos de todos los servicios públicos, peatonales, colegios y de las 4 principales plazas de San José de Mayo.</span>
                 </p>
-                <img src={ubicacionAraza} alt='ArazaUrbanicaUruguay' />
+                <img id='imgUbicacionAraza' src={ubicacionAraza} alt='ArazaUrbanicaUruguay' />
             </div>
             <div className="dvSeccionInfo">
                 <ul>
